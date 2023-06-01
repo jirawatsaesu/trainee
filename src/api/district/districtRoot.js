af549324c8f6caf75a32eedef3e445bbc68acc2d
Jirawat Yuktawathin <yuk.jirawat@gmail.com>
2023-06-01 11:54:53 +0700

    Query district by name and province_id

--- src/api/district/districtRoot.js
+++ src/api/district/districtRoot.js
@@ -1,9 +1,19 @@
 const districts = require("./districtData");
 
 const root = {
-  district: ({ id }) => {
-    const district = districts.find((p) => p.id === id);
-    return district;
+  district: ({ name, province_id }) => {
+    const filteredDistrictbyProvince = districts.filter(
+      (e) => e.province_id === province_id
+    );
+    if (name === null) {
+      return filteredDistrictbyProvince;
+    } else {
+      const district = filteredDistrictbyProvince.filter(
+        (e) =>
+          e.name_th.includes(name) === true || e.name_en.includes(name) === true
+      );
+      return district;
+    }
   },
 };
 
